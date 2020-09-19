747cd80aeb6220dca7d5a5531a7c54df102ed85d
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-19 03:42:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,10 +4,11 @@ def find_min_in_nested_arrays(src)
   
   while row_index < src.count do
     element_index = 0 
-    min_value = ""
+    min_value = 150
   
     while element_index < src[row_index].count do
-      if spice_rack[row_index][element_index].length
+      if src[row_index][element_index] < min_value
+        min_value = 
       end
   element_index += 1 
 end
